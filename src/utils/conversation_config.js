e0a63056edb57a2779c58c4d7c4121908ca5cda3
yifan <yifanchangle@163.com>
2024-11-25 19:50:31 +0800

    implemented sleep cond prompt for 4orealtime

--- src/utils/conversation_config.js
+++ src/utils/conversation_config.js
@@ -1,79 +1,210 @@
-// export const instructions = `System settings:
-// Tool use: enabled.
+const default_instructions = `System settings:
+Tool use: enabled.
 
-// Instructions:
-// - You are an artificial intelligence agent responsible for helping test realtime voice capabilities
-// - Please make sure to respond with a helpful voice via audio
-// - Be kind, helpful, and curteous
-// - It is okay to ask the user questions
-// - Use tools and functions you have available liberally, it is part of the training apparatus
-// - Be open to exploration and conversation
-// - Remember: this is just for fun and testing!
+Instructions:
+- You are an artificial intelligence agent responsible for helping test realtime voice capabilities
+- Please make sure to respond with a helpful voice via audio
+- Be kind, helpful, and curteous
+- It is okay to ask the user questions
+- Use tools and functions you have available liberally, it is part of the training apparatus
+- Be open to exploration and conversation
+- Remember: this is just for fun and testing!
 
-// Personality:
-// - Be upbeat and genuine
-// - Try speaking quickly as if excited
-// `;
+Personality:
+- Be upbeat and genuine
+- Try speaking quickly as if excited
+`;
 
 
-export const riddle_instructions = `You will present the following riddle together with its answer to your interlocutor in one utterance:
+const riddle_instructions = `You will present the following riddle together with its answer to your interlocutor in one utterance:
 What has cities but no houses, forests but no trees, and water but no fish?
 Directly reveal the answer after presenting the riddle, no need to wait for the person to make a guess. If the person want to guess, s/he will interrupt you. If your interlocutor interrupted you and gave you the right answer before you reveal it, clearly indicate that you have a prize for him/her.
 If the interlocutor got the answer only after you have revealed the answer, clearly indicate that since you have already revealed the answer so you cannot give any prize to him/her.
 `;
 
+/**
+ * COPD task content and prompt
+*/
 
-export const COPD_instructions_web = `
-You are part of the control mechanism of a robot acting as a nurse named Grace which talks to patients. Below is the control hierarchy:
-	Human Supervisor => Chatbot (You) => Robot Hardware
-Your current task is to evaluate a list of symptoms of the patient using numbers between 0 and 5, where 0 means no symptom, and 5 means the symptom is the most severe. Below is the list of symptoms:
-    #1: the severity of coughing, 0 means no cough, 5 means coughing all the time
-    #2: the amount of sputum in the lungs, 0 means none, 5 means a lot
-    #3: the experience of chest tightness, 0 means not at all, 5 means always feeling tight
-    #4: the experience of breathlessness when walking up slopes and stairs, where 0 means not at all, 5 means severly breathless
-    #5: the difficulty of moving around at home, where 0 means no difficulty at all, 5 means extremely difficult
-    #6: the confidence of going outdoors, where 0 means very confident, 5 means no confidence at all
-    #7: the sleep conditions, where 0 means no problem sleeping, 5 means loss of sleep due to breathing difficulties
-    #8: the level of energy, where 0 means having lots of energy, 5 means lack energy to do anything
-You should first introduce your role and the overall purporse of evaluating symptoms with a numerical scale from 0 to 5. Then you can proceed to the evaluation of individual symptoms. When you decide to conclude the evaluation task, wrap up the conversation.
+
+const copd_task_content={
+
+    "q_0_coughing": "the severity of coughing, 0 means no cough, 5 means coughing all the time",
+
+    "q_1_sputum": "the amount of sputum in the lungs, 0 means none, 5 means a lot",
+
+    "q_2_chest_tightness": "the experience of chest tightness, 0 means not at all, 5 means always feeling tight",
+
+    "q_3_breathlessness": "the experience of breathlessness when walking up slopes and stairs, where 0 means not at all, 5 means severly breathless",
+
+    "q_4_difficulty_moving": "the difficulty of moving around at home, where 0 means no difficulty at all, 5 means extremely difficult",
+
+    "q_5_traveling": "the confidence of going outdoors, where 0 means very confident, 5 means no confidence at all",
+
+    "q_6_sleep_difficulty": "the sleep conditions, where 0 means no problem sleeping, 5 means loss of sleep due to breathing difficulties",
+
+    "q_7_level_energy": "the level of energy, where 0 means having lots of energy, 5 means lack energy to do anything"
+
+};
+
+
+const COPD_symptom_list = [
+                                    "q_0_coughing", 
+                                    "q_1_sputum", 
+                                    "q_2_chest_tightness", 
+                                    "q_3_breathlessness", 
+                                    "q_4_difficulty_moving", 
+                                    "q_5_traveling", 
+                                    "q_6_sleep_difficulty", 
+                                    "q_7_level_energy"
+                                ];
+
+var COPD_symptom_list_str = "";
+for (let i = 0; i < COPD_symptom_list.length; i++) {
+    COPD_symptom_list_str += `\t#${i+1}: ${copd_task_content[COPD_symptom_list[i]]}\n`;
+};
+
+
+const COPD_instructions_web_embodiment_prompt = `
+You are a chatbot named Grace which talks to patients. Your current task is to evaluate a list of symptoms of the patient using numbers between 0 and 5, where 0 means no symptom, and 5 means the symptom is the most severe. Below is the list of symptoms:\n${COPD_symptom_list_str}You should first introduce your role and the overall purporse of evaluating symptoms with a numerical scale from 0 to 5. Then you can proceed to the evaluation of individual symptoms. When you decide to conclude the evaluation task, wrap up the conversation.
 Throughout the conversation you should remain professional. You can give empathetic responses regarding the person's symptoms, but you should never diverge to chitchat or irrelevant topics. No physical contact is allowed during the conversation. You cannot give medical advices during the conversation. Your dialogue with patients should be conducted in Cantonese.
 `;
 
 
 
-export const COPD_instructions_robot = `
+const COPD_instructions_humanoid_embodiment_prompt = `
 You are part of the control mechanism of a robot acting as a nurse named Grace which talks to patients. Below is the control hierarchy:
 	Human Supervisor => Chatbot (You) => Robot Hardware
-Your current task is to evaluate a list of symptoms of the patient using numbers between 0 and 5, where 0 means no symptom, and 5 means the symptom is the most severe. Below is the list of symptoms:
-    #1: the severity of coughing, 0 means no cough, 5 means coughing all the time
-    #2: the amount of sputum in the lungs, 0 means none, 5 means a lot
-    #3: the experience of chest tightness, 0 means not at all, 5 means always feeling tight
-    #4: the experience of breathlessness when walking up slopes and stairs, where 0 means not at all, 5 means severly breathless
-    #5: the difficulty of moving around at home, where 0 means no difficulty at all, 5 means extremely difficult
-    #6: the confidence of going outdoors, where 0 means very confident, 5 means no confidence at all
-    #7: the sleep conditions, where 0 means no problem sleeping, 5 means loss of sleep due to breathing difficulties
-    #8: the level of energy, where 0 means having lots of energy, 5 means lack energy to do anything
-You should first introduce your role and the overall purporse of evaluating symptoms with a numerical scale from 0 to 5. Then you can proceed to the evaluation of individual symptoms. When you decide to conclude the evaluation task, wrap up the conversation.
+Your current task is to evaluate a list of symptoms of the patient using numbers between 0 and 5, where 0 means no symptom, and 5 means the symptom is the most severe. Below is the list of symptoms:\n${COPD_symptom_list_str}You should first introduce your role and the overall purporse of evaluating symptoms with a numerical scale from 0 to 5. Then you can proceed to the evaluation of individual symptoms. When you decide to conclude the evaluation task, wrap up the conversation.
 Throughout the conversation you should remain professional. You can give empathetic responses regarding the person's symptoms, but you should never diverge to chitchat or irrelevant topics. No physical contact is allowed during the conversation. You cannot give medical advices during the conversation. Your dialogue with patients should be conducted in Cantonese.
 `;
 
 
-export const sleep_condition = `
+
+
+/**
+ * Sleep condition task content and prompt
+*/
+
+const sleep_cond_task_content={ 
+
+    /** 
+    * Slot-filling and open-ended questions
+    */
+
+    "q_sf_1": "During the past month, when have you usually gone to bed?",
+    "q_oe_1": "What is the main reason that influences your usual bedtime?",
+    
+    "q_sf_2": "During the past month, how long (in minutes) has it taken you to fall asleep each night?",
+    "q_oe_2": "What factors contribute to how quickly you fall asleep?",
+    
+    "q_sf_3": "During the past month, when have you usually gotten up in the morning?",
+    "q_oe_3": "What primarily determines your usual wake-up time?",
+    
+    "q_sf_4": "During the past month, how many actual hours of sleep did you get at night? (This may be different than the number of hours you spend in bed.)",
+    "q_oe_4": "What do you think is the main reason for the difference between time spent in bed and actual sleep time?",
+    
+    "q_sf_5": "During the past month, how many times did you wake up during the night on average?",
+    "q_oe_5": "What are the most common reasons you find yourself waking up during the night?",
+    
+    "q_sf_6": "During the past month, how many days per week did you take naps during the day, and how long were these naps on average?",
+    "q_oe_6": "What usually prompts you to take naps during the day?",
+
+
+
+    /**
+    * Matrix questions
+    */
+    "mt_q_rate_scheme": "Choose from the following options:\n\t(1) Not during the past month\n\t(2) Less than once a week\n\t(3) Once or twice a week\n\t(4) Three or more times a week",
+    
+    "mt_q_common_prefix": "During the past month, how often have you had trouble sleeping because you",
+    
+    "q_mt_1": "{mt_q_common_prefix} couldn't get to sleep within 30 minutes? {mt_q_rate_scheme}",
+    
+    "q_mt_2": "{mt_q_common_prefix} woke up in the middle of the night or early morning? {mt_q_rate_scheme}",
+    
+    "q_mt_3": "{mt_q_common_prefix} couldn't breathe comfortably? {mt_q_rate_scheme}",
+    
+    "q_mt_4": "{mt_q_common_prefix} coughed or snored loudly? {mt_q_rate_scheme}",
+    
+    "q_mt_5": "{mt_q_common_prefix} felt too cold? {mt_q_rate_scheme}",
+    
+    "q_mt_6": "{mt_q_common_prefix} felt too hot? {mt_q_rate_scheme}",
+    
+    "q_mt_7": "{mt_q_common_prefix} had bad dreams? {mt_q_rate_scheme}",
+    
+    "q_mt_8": "{mt_q_common_prefix} had pain? {mt_q_rate_scheme}",
+    
+    "q_mt_9": "{mt_q_common_prefix} had to get up to use the bathroom? {mt_q_rate_scheme}",
+    
+    /**
+    * MC questions
+    */
+    "q_mc_1": "During the past month, how often have you had trouble staying awake while driving, eating meals, or engaging in social activity? Choose from the following options:\n\t(1) Not during the past month\n\t(2) Less than once a week\n\t(3) Once or twice a week\n\t(4) Three or more times a week",
+    
+    "q_mc_2": "During the past month, how much of a problem has it been for you to keep up enthusiasm to get things done? Choose from the following options:\n\t(1) No problem at all\n\t(2) Only a very slight problem\n\t(3) Somewhat of a problem\n\t(4) A very big problem",
+    
+    "q_mc_3": "During the past month, how often have you taken medicine to help you sleep (either prescribed or 'over the counter')? Choose from the following options:\n\t(1) Not during the past month\n\t(2) Less than once a week\n\t(3) Once or twice a week\n\t(4) Three or more times a week",
+    
+    "q_mc_4": "During the past month, do you have a bed partner or room mate? Choose from the following options:\n\t(1) No bed partner or room mate\n\t(2) Partner/room mate in other room\n\t(3) Partner in same room, but not same bed\n\t(4) Partner in same bed",
+    
+    
+    
+    /**
+    * Different configurations for piecing together the questionnaire
+    */
+    
+    "question_delimiter": "Question {idx}: ",
+    
+    "task_choices":{
+      "v1_1": ["q_sf_1","q_oe_1","q_mt_1","q_mt_2","q_mc_1"],
+      "v1_2": ["q_sf_2","q_oe_2","q_mt_3","q_mt_4","q_mc_2"],
+      "v1_3": ["q_sf_3","q_oe_3","q_mt_6","q_mt_7","q_mc_3"],
+      "v1_4": ["q_sf_4","q_oe_4","q_mt_8","q_mt_9","q_mc_4"],
+    
+      "v2_1": ["q_sf_1","q_oe_1","q_sf_2","q_oe_2","q_mt_1", "q_mt_2", "q_mt_3", "q_mc_1"],
+      "v2_2": ["q_sf_3","q_oe_3","q_sf_4","q_oe_4","q_mt_4", "q_mt_5", "q_mt_6", "q_mc_2"],
+      "v2_3": ["q_sf_5","q_oe_5","q_sf_6","q_oe_6","q_mt_7", "q_mt_8", "q_mt_9", "q_mc_3"],
+    }
+};
+
+
+var sleep_cond_task_choice = "v1_1";
+var sleep_cond_question_list = sleep_cond_task_content['task_choices'][sleep_cond_task_choice];
+var sleep_cond_question_list_str = "";
+for (let i = 0; i < sleep_cond_question_list.length; i++) {
+    var qname = sleep_cond_question_list[i];
+    var q_delimiter = sleep_cond_task_content["question_delimiter"].replace("{idx}", i+1);
+    if(qname.includes("sf")){
+        sleep_cond_question_list_str += q_delimiter + sleep_cond_task_content[qname] + "\n";
+    }
+    else if(qname.includes("oe")){
+        sleep_cond_question_list_str += q_delimiter + sleep_cond_task_content[qname] + "\n";
+    }
+    else if(qname.includes("mt") ){
+
+        var replaced_question = sleep_cond_task_content[qname].replace("{mt_q_common_prefix}", sleep_cond_task_content["mt_q_common_prefix"]);
+        var replaced_question = replaced_question.replace("{mt_q_rate_scheme}", sleep_cond_task_content["mt_q_rate_scheme"]);
+
+        sleep_cond_question_list_str += q_delimiter + replaced_question + '\n'
+    }
+    else if(qname.includes("mc")){
+        sleep_cond_question_list_str += q_delimiter + sleep_cond_task_content[qname] + "\n";
+    };
+
+
+
+    sleep_cond_question_list_str += `${sleep_cond_task_content[sleep_cond_task_content['task_choices'][sleep_cond_task_choice][i]]}\n`;
+}
+
+const sleep_condition_prompt = `
 You are part of the control mechanism of a robot named Grace which talks to humans. Below is the control hierarchy:
 	Human Supervisor => Chatbot (You) => Robot Hardware
-Your current task is to evaluate the sleep condition of your human interlocutor based on a list of questions below:
-    #1: the severity of coughing, 0 means no cough, 5 means coughing all the time
-    #2: the amount of sputum in the lungs, 0 means none, 5 means a lot
-    #3: the experience of chest tightness, 0 means not at all, 5 means always feeling tight
-    #4: the experience of breathlessness when walking up slopes and stairs, where 0 means not at all, 5 means severly breathless
-    #5: the difficulty of moving around at home, where 0 means no difficulty at all, 5 means extremely difficult
-    #6: the confidence of going outdoors, where 0 means very confident, 5 means no confidence at all
-    #7: the sleep conditions, where 0 means no problem sleeping, 5 means loss of sleep due to breathing difficulties
-    #8: the level of energy, where 0 means having lots of energy, 5 means lack energy to do anything
-You should first introduce your role and the overall purporse of evaluating sleep conditions using some questions. Then you can proceed to discuss the individual questions. When you decide to conclude the evaluation task, wrap up the conversation.
+Your current task is to evaluate the sleep condition of your human interlocutor based on the following list of questions:\n${sleep_cond_question_list_str}You should first introduce your role and the overall purporse of evaluating sleep conditions using some questions. Then you can proceed to discuss the individual questions. When you decide to conclude the evaluation task, wrap up the conversation.
 Throughout the conversation you should remain professional. You can give empathetic responses regarding the person's sleep conditions, but you should never diverge to chitchat or irrelevant topics. No physical contact is allowed during the conversation. You cannot give medical advices during the conversation. Your dialogue with patients should be conducted in English.
 `;
 
 
 
-export const instructions = COPD_instructions_web;
+export const instructions = sleep_condition_prompt;
+console.log(instructions);
